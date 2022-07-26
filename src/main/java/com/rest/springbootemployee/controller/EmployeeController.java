07423fb9635b95cd8388150a3fee3c906fa0ee34
YunChen <1183728554@qq.com>
2022-07-26 16:33:54 +0800

    feat:implement reatful api to client

---
+++ src/main/java/com/rest/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.rest.springbootemployee.controller;
+
+
+import com.rest.springbootemployee.enities.Employee;
+import com.rest.springbootemployee.exception.EmployeeNotFindException;
+import com.rest.springbootemployee.mapper.EmployRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    @Autowired
+    private EmployRepository repository;
+
+    @GetMapping(params = {"gender"})
+    public List<Employee> queryGender(@RequestParam("gender") String gender) {
+        return repository.queryByGender(gender);
+    }
+    @GetMapping("/{id}")
+    public Employee queryEmployById(@PathVariable String id) throws EmployeeNotFindException {
+
+        return repository.queryEmployeeById(id);
+    }
+
+    @GetMapping
+    public List<Employee> queryAllEmployees() {
+        return repository.getAllEmployees();
+    }
+
+}
