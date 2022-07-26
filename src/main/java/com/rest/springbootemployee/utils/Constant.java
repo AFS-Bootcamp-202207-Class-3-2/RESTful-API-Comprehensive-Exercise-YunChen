b94a8d6e712a27f9d47d99cecc28a04cfe491359
YunChen <1183728554@qq.com>
2022-07-26 16:56:03 +0800

    feat:add constant in utils package

---
+++ src/main/java/com/rest/springbootemployee/utils/Constant.java
@@ -0,0 +1,7 @@
+package com.rest.springbootemployee.utils;
+
+public interface Constant {
+    String EMPLOYEE_IS_EXIST = "This Employee is exist";
+    String EMPLOYEE_IS_NOT_EXIST = "This Employee is not exist";
+
+}
