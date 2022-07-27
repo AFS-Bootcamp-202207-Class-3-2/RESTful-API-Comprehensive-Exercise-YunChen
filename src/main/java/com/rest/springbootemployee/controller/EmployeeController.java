a197ac5f0a3da4c36d6730dd814f7142d94fdc60
YunChen <1183728554@qq.com>
2022-07-27 16:19:57 +0800

    feat:replace repository to employee service and test controller PASSED

--- src/main/java/com/rest/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/rest/springbootemployee/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.rest.springbootemployee.controller;
 
 import com.rest.springbootemployee.enities.Employee;
 import com.rest.springbootemployee.mapper.EmployeeRepository;
+import com.rest.springbootemployee.services.EmployeeService;
 import com.rest.springbootemployee.utils.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,41 +15,41 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
 
     @GetMapping(params = {"gender"})
     public List<Employee> queryGender(@RequestParam("gender") String gender) {
-        return employeeRepository.queryByGender(gender);
+        return employeeService.queryEmployeeByGender(gender);
     }
 
     @GetMapping("/{id}")
     public Employee queryEmployeeById(@PathVariable String id)   {
-        return employeeRepository.queryEmployeeById(id);
+        return employeeService.queryEmployeeById(id);
     }
 
     @GetMapping
     public List<Employee> queryAllEmployees() {
-        return employeeRepository.getAllEmployees();
+        return employeeService.findALl();
     }
 
     @GetMapping(params = {"page", "pageSize"})
     public List<Employee> findByPage(@RequestParam("page") int page,
                                      @RequestParam("pageSize") int pageSize) {
-        return employeeRepository.findByPage(page, pageSize);
+        return employeeService.findEmployeeByPage(page, pageSize);
     }
 
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Employee addAnEmployee(@RequestBody Employee employee) {
-        employeeRepository.insert(employee);
+        employeeService.insertEmployee(employee);
         return employee;
     }
 
     @PutMapping("/{id}")
     public String updateAnEmployee(@PathVariable("id") String employeeId,
                                    @RequestBody Employee employee) {
-        return employeeRepository.updateAnEmployee(employeeId,employee)?
+        return employeeService.update(employeeId,employee)?
                 Constant.UPDATE_EMPLOYEE_SUCCESS:
                 Constant.EMPLOYEE_IS_NOT_EXIST;
     }
@@ -56,7 +57,7 @@ public class EmployeeController {
     @DeleteMapping("/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteAnEmployee(@PathVariable String id) {
-        employeeRepository.deleteEmployee(id);
+        employeeService.removeEmployee(id);
     }
 
 }
