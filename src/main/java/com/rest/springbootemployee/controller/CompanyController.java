4812d416d74064c026d17cf7c32bec9893c3eea2
YunChen <1183728554@qq.com>
2022-07-27 21:14:03 +0800

    fix:replace repository of service in companies controller

--- src/main/java/com/rest/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/rest/springbootemployee/controller/CompanyController.java
@@ -2,7 +2,7 @@ package com.rest.springbootemployee.controller;
 
 import com.rest.springbootemployee.enities.Company;
 import com.rest.springbootemployee.enities.Employee;
-import com.rest.springbootemployee.mapper.CompaniesRepository;
+import com.rest.springbootemployee.services.CompaniesService;
 import com.rest.springbootemployee.utils.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,47 +13,48 @@ import java.util.List;
 @RequestMapping("/companies")
 public class CompanyController {
 
+
     @Autowired
-    private CompaniesRepository companiesRepository;
+    private CompaniesService companiesService;
 
     @GetMapping
     public List<Company> queryAllCompanies() {
-        return companiesRepository.queryAllCompanies();
+        return companiesService.queryAllCompanies();
     }
 
     @GetMapping("/{id}")
     public Company queryCompanyById(@PathVariable String id) {
-        return companiesRepository.queryCompanyById(id);
+        return companiesService.queryCompanyById(id);
     }
 
     @GetMapping("/{id}/employees")
     public List<Employee> queryEmployeesInCompanyById(@PathVariable String id) {
-        return companiesRepository.queryEmployeesInCompanyById(id);
+        return companiesService.queryEmployeesInCompanyById(id);
     }
 
     @GetMapping(params = {"page", "pageSize"})
     public List<Company> queryCompanyPage(@RequestParam("page") int page,
                                           @RequestParam("pageSize") int pageSize) {
-        return companiesRepository.queryCompanyPage(page, pageSize);
+        return companiesService.queryCompanyPage(page, pageSize);
     }
 
     @PostMapping
     public String insertCompany(@RequestBody Company company) {
-        companiesRepository.insertCompany(company);
+        companiesService.insertCompany(company);
         return Constant.ADD_COMPANY_SUCCESS;
     }
 
     @PutMapping("/{id}")
     public String updateCompany(@PathVariable("id") String companyId,
                                 @RequestBody Company company) {
-        return companiesRepository.updateCompany(companyId, company) ?
+        return companiesService.updateCompany(companyId, company) ?
                 Constant.UPDATE_COMPANY_SUCCESS :
                 Constant.COMPANY_IS_NOT_EXIST;
     }
 
     @DeleteMapping("/{id}")
     public String deleteCompanyById(@PathVariable("id") String companyId) {
-        boolean isSuccessDelete = companiesRepository.deleteCompanyById(companyId);
+        boolean isSuccessDelete = companiesService.deleteCompanyById(companyId);
         return isSuccessDelete ?
                 Constant.DELETE_COMPANY_SUCCESS :
                 Constant.COMPANY_IS_NOT_EXIST;
