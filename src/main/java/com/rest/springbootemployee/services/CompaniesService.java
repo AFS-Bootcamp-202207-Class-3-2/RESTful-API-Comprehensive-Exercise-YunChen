111b1c11d76cef0b143ec53a5940db4ec9e4cfb0
YunChen <1183728554@qq.com>
2022-07-27 20:40:18 +0800

    test:Company Service should_get_all_companies_when_query_all_given_request PASSED

--- src/main/java/com/rest/springbootemployee/services/CompaniesService.java
+++ src/main/java/com/rest/springbootemployee/services/CompaniesService.java
@@ -1,7 +1,20 @@
 package com.rest.springbootemployee.services;
 
+import com.rest.springbootemployee.enities.Company;
+import com.rest.springbootemployee.mapper.CompaniesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CompaniesService {
+
+
+    @Autowired
+    private CompaniesRepository companiesRepository;
+    public List<Company> queryAllCompanies() {
+        return companiesRepository.queryAllCompanies();
+    }
+
 }
--- src/test/java/com/rest/springbootemployee/services/CompaniesServiceTest.java
+++ src/test/java/com/rest/springbootemployee/services/CompaniesServiceTest.java
@@ -1,10 +1,19 @@
 package com.rest.springbootemployee.services;
 
+import com.rest.springbootemployee.enities.Company;
 import com.rest.springbootemployee.mapper.CompaniesRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
 public class CompaniesServiceTest {
@@ -12,4 +21,26 @@ public class CompaniesServiceTest {
     CompaniesRepository companiesRepository;
     @InjectMocks
     CompaniesService companiesService;
+    @BeforeEach
+    void initData() {
+        companiesRepository.setNextId("1");
+        companiesRepository.getCompanies().clear();
+    }
+
+    @Test
+    void should_get_all_companies_when_query_all_given_request()throws Exception {
+        //given
+        List<Company> exceptCompanies = new ArrayList<>();
+        Company firstCompany = new Company("1", "oocl", new ArrayList<>());
+        Company secondCompany = new Company("1", "oocl", new ArrayList<>());
+        Company thirdCompany = new Company("1", "oocl", new ArrayList<>());
+        exceptCompanies.add(firstCompany);
+        exceptCompanies.add(secondCompany);
+        exceptCompanies.add(thirdCompany);
+        //when
+        given(companiesRepository.queryAllCompanies()).willReturn(exceptCompanies);
+        //then
+        List<Company> companiesFromDb = companiesService.queryAllCompanies();
+        assertThat(companiesFromDb).isEqualTo(exceptCompanies);
+    }
 }
